43c0b5c51335601c54cd6b995ef43bda6bc4fc1c
syedahalam <syedahalam@yahoo.com>
2021-09-08 16:11:10 -0400

    added media query for styling on different browser sizes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,47 @@ import './App.css'
 import SearchForm from './components/SearchForm';
 import SearchResults from './components/SearchResults';
 
+const images = [
+	{
+		id: 1,
+		url: 'https://media.giphy.com/media/3DsNP07nApt1eEyjvM/giphy.gif',
+	},
+	{
+		id: 2,
+		url: 'https://media1.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif',
+	},
+	{
+		id: 3,
+		url: 'https://www.thisiscolossal.com/wp-content/uploads/2018/04/agif2opt.gif',
+	},
+	{
+		id: 4,
+		url: 'https://media.giphy.com/media/3oEdva9BUHPIs2SkGk/giphy.gif',
+	},
+	{
+		id: 5,
+		url: 'https://media.giphy.com/media/3o6ozoD1ByqYv7ARIk/giphy.gif',
+	},
+	{
+		id: 6,
+		url: 'https://buffer.com/library/wp-content/uploads/2016/06/giphy.gif',
+	},
+	{
+		id: 7,
+		url: 'https://media4.giphy.com/avatars/default5.gif',
+	},
+	{
+		id: 8,
+		url: 'https://media2.giphy.com/media/3oEduPff5ErjNmlbwY/source.gif',
+	},
+];
 
 function App() {
   return (
 		<div className='App'>
 			<h1>🦸‍♀️Giphy Searcher🦸‍♀️</h1>
       <SearchForm />
-      <SearchResults />
+      <SearchResults images={images} />
 		</div>
 	);
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Search.css'
 
-const SearchResults = () => {
+const SearchResults = ({images}) => {
+    //return early if there are no images
+    if(!images.length){
+        return <h2>No Images Found!</h2>
+    }
     return (
-        <div>
-            <h2>Search Results</h2>
+        <div className='gallery'>
+            {images.map(image =>(
+                <div key={image.id}
+                className='gif'>
+                    <img src={image.url} alt='images'/>
+                </div>
+            ))}
         </div>
     );
 };
